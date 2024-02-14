e6226c694804d0934b37c38d535a7c8320b3d5a1
eziur <eziur.wz@gmail.com>
2024-02-14 01:02:26 -0800

    Manual role assignment UX

--- commands/utility/assign_roles.js
+++ commands/utility/assign_roles.js
@@ -1,4 +1,4 @@
-const { ActionRowBuilder, ButtonBuilder, ButtonStyle, InteractionCollector, InteractionResponse, ComponentType, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder, UserSelectMenuBuilder, RoleSelectMenuBuilder } = require('discord.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, SlashCommandBuilder, UserSelectMenuBuilder, RoleSelectMenuBuilder, userMention, roleMention } = require('discord.js');
 
 async function manualRoleAssignment(i) {
 	const userSelect = new UserSelectMenuBuilder()
@@ -21,30 +21,50 @@ async function manualRoleAssignment(i) {
 
 	const userSelectRow = new ActionRowBuilder()
 		.addComponents(userSelect);
-
 	const roleSelectRow = new ActionRowBuilder()
 		.addComponents(roleSelect);
-
 	const buttonRow = new ActionRowBuilder()
 		.addComponents(confirm);
 
 	const response = await i.update({
 		content: 'Manual role assignment',
 		components: [userSelectRow, roleSelectRow, buttonRow],
-		system.log
 	});
 
-	// filter: is from userSelect
-	// const userCollectorFilter = userSelectInteraction => 
+	const userCollector = response.createMessageComponentCollector({ componentType: ComponentType.UserSelect, time: 1_200_000 });
+	userCollector.on('collect', async userInput => {
+		await userInput.deferReply({ ephemeral: true });
 
-	// filter: is from roleSelect
+		if (userCollector.collected.at(0).replied) {
+			userCollector.collected.at(0).deleteReply();
+			userCollector.collected.delete(userCollector.collected.firstKey());
+		}
 
-	// filter: is from confirm
+		let currentlySelectedUsers = `Currently Selected Users: `;
+		for (let j = 0; j < userInput.values.length; j++) {
+			currentlySelectedUsers += `${userMention(userInput.values[j])}, `;
+		}
 
-	// const userCollector = response.createMessageComponentCollector({ componentType: ComponentType.StringSelect, time: 3_600_000 });
+		await userInput.editReply(currentlySelectedUsers);
+	});
+
+	const roleCollector = response.createMessageComponentCollector({ componentType: ComponentType.RoleSelect, time: 1_200_000 });
+	roleCollector.on('collect', async roleInput => {
+		await roleInput.deferReply({ ephemeral: true });
+
+		// if (roleCollector.collected.at(0).replied) {
+		// 	roleCollector.collected.at(0).deleteReply();
+		// 	roleCollector.collected.delete(roleCollector.collected.firstKey());
+		// }
+
+		let currentlySelectedRoles = `Currently Selected Roles: `;
+		for (let j = 0; j < roleInput.values.length; j++) {
+			currentlySelectedRoles += `${roleMention(roleInput.values[j].id.toString())}, `;
+		}
+
+		await roleInput.editReply(currentlySelectedRoles);
+	});
 	// const buttonCollector = response.createMessageComponentCollector({ componentType: ComponentType.Button, time: 3_600_000 });
-
-	// userCollector.on('collect', async i => {
 
 	// });
 }
@@ -87,7 +107,6 @@ module.exports = {
 				manualRoleAssignment(i);
 			}
 		});
-
 			// if (response.isStringSelectMenu()) {
 			// 	if (response.customId === 'select_mode') {
 			// 		const mode = interaction.values[0];
